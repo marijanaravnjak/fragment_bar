ecb7f78d4e89a347d402619ee608e0cb6945fd9c
Marijana Ravnjak <marijana@24setup.com>
2023-12-08 10:28:57 +0100

    feat(#15): Create style in gallery

--- src/components/Gallery/Photo/Photo.jsx
+++ src/components/Gallery/Photo/Photo.jsx
@@ -2,27 +2,63 @@ import React from 'react'
 import { Box, ImageList, ImageListItem } from '@mui/material'
 
 const Photo = (photo) => {
+
     return (
         <Box sx={{
             width: 470,
             height: 350,
-            background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 100%, ' +
-                'rgb(245,245,245) 10%, rgba(0,0,0,0) 100%)',
-                borderRadius: '10%'
+            ml:2
         }}>
             <ImageList
+                key={photo.id}
                 sx={{
                     width: 600,
                     height: 350,
-                    ml: 10
+                    ml: '10',
+                    overflow: 'hidden',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    zIndex: '999',
+                    cursor: 'pointer',
+                    WebkitTransitionProperty: 'all',
+                    WebkitTransitionDuration: '0.7s',
+                    WebkitTransitionTimingFunction: 'ease',
+                    '&:hover': {
+                        position: 'absolute',
+                        transform: 'scale(2)',
+                        zIndex: 2,
+                        overflow: 'visible',
+                        height: '250px',
+                        width: '250px',
+                        margin: '10 auto',
+                        animationName: 'stretch',
+                        animationDuration: '1.5s',
+                        animationTimingFunction: 'ease-out',
+                        animationDelay: '0',
+                        animationDirection: 'alternate',
+                        animationIterationCount: 'infinite',
+                        animationFillMode: 'none',
+                        animationPlayState: 'running',
+                    },
+
+                    '&:@keyframes stretch': {
+                        '0%': {
+                            transform: 'scale(.3)',
+                        },
+                        '100%': {
+                            transform: 'scale(2.5)',
+                        },
+                    },
                 }}>
                 <ImageListItem key={photo.img}>
                     <img
                         src={photo.src}
-                        alt={photo.title} />
+                        alt={photo.title}
+                    />
                 </ImageListItem>
             </ImageList>
-        </Box>
+        </Box >
     )
 }
 
