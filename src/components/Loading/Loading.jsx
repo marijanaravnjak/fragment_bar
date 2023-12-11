27db51dc6a5c53a3f3a4236171e022937efc1512
Marijana Ravnjak <marijana@24setup.com>
2023-12-11 17:26:46 +0100

    feat(#20): Add loading in contact map

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Box, CircularProgress, Skeleton } from '@mui/material'
+import PropTypes from 'prop-types'
+
+
+const Loading = ({ loading }) => {
+    
+    return (
+
+        <Box sx={{
+            display: 'flex',
+            width: 500,
+            height: 500,
+            background: 'linear-gradient(#f07e6e, #84cdfa, #5ad1cd)',
+            opacity: 0.7
+        }}>
+            {loading &&
+                <><Skeleton  variant='rectangular'    animation='wave' />
+                    <CircularProgress style={{ width: 300, height: 300, margin: '18%' }} />
+                </>}
+        </Box>
+    )
+}
+Loading.propTypes = {
+    loading: PropTypes.bool
+}
+Loading.defaultProps = {
+    loading: false
+}
+export default Loading
