182427f35e61687750ba8805bcd6534847a9b27b
Marijana Ravnjak <marijana@24setup.com>
2023-12-13 15:03:34 +0100

    Feat(#7): Create About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material'
+
 
 const About = () => {
+
     return (
-        <div>About</div>
+        <Grid container>
+            <Card sx={{ display: 'flex', flexDirection:'row', backgroundColor:'ghostwhite'}}>
+                <CardContent >
+                    <Grid item xs={12} sm={6} md={6} lg>
+                        <Typography component='div' variant='h4' fontFamily='cursive' noWrap>
+                            FRAGMENT BAR – DAN POČINJE OVDE
+                        </Typography><br />
+                        <Typography variant='h5' color='text.secondary' component='div'>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom>Fragment je spoj udobnog kafića i kluba, na odličnoj lokaciji, u Kuzminskoj ulici.</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom>Izdvaja enterijerom koji je mešavina umetničke galerije i kafića koje obilazimo u velikim gradovima.</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom>Bilo da ste tu zbog kafe sa prijateljima ili koktela sa društvom,</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom>biće vam udobno i prijatno u ovom ušuškanom ambijentu.</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom> Svetlo je blago, prigušeno, pa je idealan izbor za sve dnevne varijante:</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom> druženje sa prijateljicama, ili muško okupljanje,</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom>pijuckanje kafe uz dnevni posao na kompu,</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom>brzinska kafica i piće u toku dana, a cene su fantastične…</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom>Fragment je popularno  mesto u sremskomitrovačkom krugu izlazaka.</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom> Jedinstveno mesto u kojem se, već na prvi pogled, vidi da se mislilo na sve detalje.</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom> Kafić u kojem je kafa uvek dobra, a konobari znaju više o nama od nekih prijatelja.</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom>Često Fragment završi na mnogim Instagram profilima,</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom>fotke se dele...</Typography>
+                            <Typography variant='h5' fontFamily='cursive' gutterBottom> I tako je Fragmet zaslužio svoje mesto među „izabranima“...</Typography>
+                            <Typography variant='overline'>(izvor https://www.ozon.rs/uncategorized/2021/fragment-bar-dan-pocinje-ovde/)</Typography>
+                        </Typography>
+                    </Grid>
+                </CardContent>
+                <CardContent>
+                    <CardMedia sx={{ minHeight: 800, minWidth: 300}}
+                        component='video'
+                        src='fragment.mp4'
+                        controls>
+                    </CardMedia>
+                </CardContent>
+            </Card >
+        </Grid>
     )
 }
+
 
 export default About
